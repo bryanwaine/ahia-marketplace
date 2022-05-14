d58fb1d7eea945ddec7f48d82c7d19b9647d26c9
bryan waine <nwanne.ezeaka@gmail.com>
2022-05-14 16:46:10 +0100

    added password reset service

--- pages/check_email.js
+++ pages/check_email.js
@@ -9,48 +9,54 @@ const Check_email = () => {
   const classes = useStyles();
   return (
     <Layout title='Check your email for further Instructions'>
-      <Typography component='h1' variant='h1'>
-        Reset password
-      </Typography>
-      <Grid
-        container
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          marginTop: '5rem',
-        }}
-      >
-        <Grid item md={4} xs={12}>
-          <Card
-            style={{
-              margin: 10,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              padding: 20,
-            }}
-          >
-                      <DoneAllIcon fontSize='large' style={{fontSize: '5rem'}}color='success' />
-            <Typography style={{ marginTop: 20 }}>
-              Check your registered email address for further instructions on
-              how to reset your password.
-            </Typography>
-          </Card>
-        </Grid>
-
-        <NextLink href={`/login`} passHref>
-          <Link>
-            <Typography
-              variant='h6'
-              className={classes.centeredText}
-              style={{ color: '#ff0000' }}
+      <div className={classes.form}>
+        <Typography component='h1' variant='h1'>
+          Reset password
+        </Typography>
+        <Grid
+          container
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            marginTop: '5rem',
+          }}
+        >
+          <Grid item md={9} xs={12}>
+            <Card
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                padding: 20,
+                margin: 10,
+              }}
             >
-              Back to sign in
-            </Typography>
-          </Link>
-        </NextLink>
-      </Grid>
+              <DoneAllIcon
+                fontSize='large'
+                style={{ fontSize: '5rem' }}
+                color='success'
+              />
+              <Typography style={{ marginTop: 20 }}>
+                Check your registered email address for further instructions on
+                how to reset your password.
+              </Typography>
+            </Card>
+          </Grid>
+
+          <NextLink href={`/login`} passHref>
+            <Link>
+              <Typography
+                variant='h6'
+                className={classes.centeredText}
+                style={{ color: '#ff0000' }}
+              >
+                Back to sign in
+              </Typography>
+            </Link>
+          </NextLink>
+        </Grid>
+      </div>
     </Layout>
   );
 };
